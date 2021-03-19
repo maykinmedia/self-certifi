f0d58950083f9cd8af00f78278df5f253bb4fd78
Sergei Maertens <sergei@maykinmedia.nl>
2021-03-19 16:12:51 +0100

    :white_check_mark: Added tests for module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,18 @@
-# import pytest
+import os
+
+import pytest
 
 
-# @pytest.fixture
-# def some_fixture(request):
-#     return "foo"
+@pytest.fixture
+def clean_environ(request):
+    envvars = ("EXTRA_VERIFY_CERTS", "REQUESTS_CA_BUNDLE")
+
+    def reset():
+        for envvar in envvars:
+            if envvar not in os.environ:
+                continue
+            del os.environ[envvar]
+
+    reset()
+    yield os.environ
+    reset()
---
+++ tests/test_loading.py
@@ -0,0 +1,63 @@
+import os
+
+import certifi
+
+from self_certifi import load_self_signed_certs
+
+CERTS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "certs")
+
+
+def test_no_extra_certs(tmp_path, clean_environ):
+    load_self_signed_certs(tmp_path)
+
+    assert "REQUESTS_CA_BUNDLE" not in os.environ
+    assert len([x for x in tmp_path.iterdir()]) == 0
+
+
+def test_explicit_certs_list(tmp_path, clean_environ):
+    certs = [
+        os.path.join(CERTS_DIR, "root1.crt"),
+        os.path.join(CERTS_DIR, "cert2.crt"),
+    ]
+
+    load_self_signed_certs(tmp_path, paths=certs)
+
+    assert "REQUESTS_CA_BUNDLE" in os.environ
+    bundle_file = os.environ["REQUESTS_CA_BUNDLE"]
+    children = [str(x) for x in tmp_path.iterdir()]
+    assert children == [bundle_file]
+
+    with open(bundle_file, "r") as infile:
+        bundle = infile.read()
+
+    for cert in certs:
+        with open(cert, "r") as cert_file:
+            assert cert_file.read() in bundle
+
+    with open(certifi.where(), "r") as certifi_bundle:
+        assert certifi_bundle.read() in bundle
+
+
+def test_envvar_list(tmp_path, clean_environ):
+    certs = [
+        os.path.join(CERTS_DIR, "root1.crt"),
+        os.path.join(CERTS_DIR, "cert2.crt"),
+    ]
+    clean_environ["EXTRA_VERIFY_CERTS"] = ",".join(certs)
+
+    load_self_signed_certs(tmp_path)
+
+    assert "REQUESTS_CA_BUNDLE" in os.environ
+    bundle_file = os.environ["REQUESTS_CA_BUNDLE"]
+    children = [str(x) for x in tmp_path.iterdir()]
+    assert children == [bundle_file]
+
+    with open(bundle_file, "r") as infile:
+        bundle = infile.read()
+
+    for cert in certs:
+        with open(cert, "r") as cert_file:
+            assert cert_file.read() in bundle
+
+    with open(certifi.where(), "r") as certifi_bundle:
+        assert certifi_bundle.read() in bundle
